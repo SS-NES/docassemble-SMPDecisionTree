88fc715493aa952588af644f40efffebe096c78a
System Administrator <o.mussmann@esciencecenter.nl>
2024-06-03 11:26:40 +0200

    test commit to new org

---
+++ README.md
@@ -0,0 +1,3 @@
+# Software Management Plan
+
+This is a questionnaire package for a Software Management Plan (SMP). It is used with the `docassemble` framework: https://docassemble.org .
---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.SPMDecisionTree',
+      version='0.0.2',
+      description=('A Software Management Plan questionnaire for docassemble'),
+      long_description='# Software Management Plan\r\n\r\nThis is a questionnaire package for a Software Management Plan (SMP). It is used with the `docassemble` framework: https://docassemble.org .',
+      long_description_content_type='text/markdown',
+      author='Ole Mussmann',
+      author_email='github+account@ole.mn',
+      license='Apache-2.0',
+      url='https://smp.ole.mn',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/SPMDecisionTree/', package='docassemble.SPMDecisionTree'),
+     )
+
